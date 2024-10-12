2b0d74a915cac275778695f931186aa7738d211b
Bui Dinh Kha <buidinhkha@gmail.com>
2024-10-13 01:19:10 +0700

    feat: set url certificate for card

--- app/Observers/SettingObserver.php
+++ app/Observers/SettingObserver.php
@@ -27,27 +27,6 @@ class SettingObserver
                 }
             }
         }
-
-        if (!empty($setting->getChanges()) && in_array($setting->key, ['place_occupational', 'complete_to', 'signature_photo_occupational', 'place_occupational', 'director_name_occupational', 'effective_to'])) {
-            $certificateOccupations = Certificate::select('id')->where('type', CertificateConstant::OCCUPATIONAL_SAFETY)->get();
-            foreach ($certificateOccupations as $certificate) {
-                dispatch(new CreateImageCertificate($certificate->id))->onQueue('default');
-            }
-        }
-
-        if (!empty($setting->getChanges()) && in_array($setting->key, ['director_name_electric', 'signature_photo_electric'])) {
-            $certificateOccupations = Certificate::select('id')->where('type', CertificateConstant::ELECTRICAL_SAFETY)->get();
-            foreach ($certificateOccupations as $certificate) {
-                dispatch(new CreateImageCertificate($certificate->id))->onQueue('default');
-            }
-        }
-
-        if (!empty($setting->getChanges()) && in_array($setting->key, ['work_unit', 'place_paper', 'director_name_paper', 'signature_photo_paper'])) {
-            $certificateOccupations = Certificate::select('id')->where('type', CertificateConstant::PAPER_SAFETY)->get();
-            foreach ($certificateOccupations as $certificate) {
-                dispatch(new CreateImageCertificate($certificate->id))->onQueue('default');
-            }
-        }
     }
     public function deleted(Setting $setting)
     {
